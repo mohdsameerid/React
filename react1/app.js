9a7daeabf5f398d0ff2361cb62c85d0d653c4df6
mohdsameerid <mohdsameeredu@gmail.com>
2023-01-20 02:36:09 -0800

    working till 2

--- react1/app.js
+++ react1/app.js
@@ -1,9 +1,13 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", { id: "title" } , "Hello Everyone");
      
-    const heading2 = React.createElement("h2", { id: "title" } , "hello React");
+const heading2 = React.createElement("h2", { id: "title" } , "hello React We are Learning ");
 
-    const container = React.createElement("div" , {id: "container"} , [heading, heading2]);
+const container = React.createElement("div" , {id: "container"} , [heading, heading2]);
 
-    const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    root.render(container);
+root.render(container);
