3498801f567674c68e572a8b444e6610787fcba4
mohdsameerid <mohdsameeredu@gmail.com>
2023-01-21 05:53:19 -0800

    completed upto ass 3

--- react1/app.js
+++ react1/app.js
@@ -9,30 +9,47 @@ import ReactDOM from "react-dom/client";
 
 
 // JSX 
-const header = ( 
-    <div className="title">
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-    </div>
-);
+// const header = ( 
+//     <div className="title">
+//         <h1>Heading 1</h1>
+//         <h2>Heading 2</h2>
+//         <h3>Heading 3</h3>
+//     </div>
+// );
 
-function Welcome(props){
-    return <h1> Welcome , {props.name}</h1>
-}
+// function Welcome(props){
+//     return <h1> Welcome , {props.name}</h1>
+// }
 
-const Header2 = () => {
-    return (
-    <div className="title">
-        <Welcome name="Samir" />
-        <Welcome name="Haseen" />
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-    </div>
+// // fuctional components
+// const Header2 = () => {
+//     return (
+//     <div className="title">
+//         <Welcome name="Samir" />
+//         <Welcome name="Haseen" />
+//         <h1>Heading 1</h1>
+//         <h2>Heading 2</h2>
+//         <h3>Heading 3</h3>
+//     </div>
+//     );
+// }
+
+
+
+const Header = () => {
+    return(
+        <div className="header">
+            <h3>Header</h3>
+            <ul className="menu">
+                <li>About </li>
+                <li>Contact </li>
+                <li>Services </li>
+            </ul>
+        </div>
     );
 }
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(Header2());
+root.render(Header());
